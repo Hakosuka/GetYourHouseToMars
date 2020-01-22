da5fd4aa65d2d5b1ff11d670b0ac159d49982b76
Hakosuka <ciarancumiskey18594@gmail.com>
2020-01-22 15:43:23 +0000

    Added some string formatting to improve the UI using transformation maps

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -36,5 +36,5 @@ data class MarsProperty(
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
         val price: Double) : Parcelable {
-
+    val isRental get() = type == "rent"
 }
